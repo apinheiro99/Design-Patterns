0c18ca2ca8e2cf26754e89cf5fcde32519e13767
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-02 17:49:26 -0500

    Completing class Calculador_de_impostos

--- Calculador_de_impostos.py
+++ Calculador_de_impostos.py
@@ -1,4 +1,14 @@
 class Calculador_de_impostos (object):
+    
     def realiza_calculo(self, orcamento):
         imposto_calculado = orcamento.valor * 0.1
-        
+        print (imposto_calculado)
+
+#Testa a classe dentro dela mesma
+if __name__ == "__main__":
+    from Orcamento import Orcamento
+
+    calculador = Calculador_de_impostos()
+    orcamento = Orcamento(500)
+    calculador.realiza_calculo(orcamento)
+
