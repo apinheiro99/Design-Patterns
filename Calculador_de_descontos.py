59df30353a02bcf07fe74a8e1298b1bda883a8b5
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-11 22:19:41 -0500

    Implementando o teste da classe Calculador de descontos

--- Calculador_de_descontos.py
+++ Calculador_de_descontos.py
@@ -5,3 +5,15 @@ class Calculador_de_descontos(object):
             return orcamento.valor * 0.1
         elif orcamento.valor > 500:
             return orcamento.valor * 0.07
+
+#Testa a classe dentro dela mesma
+if __name__ == "__main__":
+
+    from Orcamento import Orcamento, Item
+
+    orcamento = Orcamento()
+    orcamento.adiciona_itens(Item('ITEM -1', 100))
+    orcamento.adiciona_itens(Item('ITEM -2', 50)) 
+    orcamento.adiciona_itens(Item('ITEM -3', 400))
+
+    print (orcamento.valor)
