a9ab87111e4ebe109c4c7516beca8d5863cd7eaa
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-14 20:18:36 -0500

    Teste Classe Calculador_de_impostos

--- test_calculador.py
+++ test_calculador.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
-# from Descontos import *
-from Orcamento import *
-from Calculador_de_descontos import *
 
 class Test_Calculador(TestCase):
     
     def test_Classe_Calculador_de_desconto(self):
+        from Calculador_de_descontos import Calculador_de_descontos
+        from Orcamento import Orcamento, Item
 
         orcamento = Orcamento()
         orcamento.adiciona_itens(Item('ITEM -1', 100))
@@ -20,3 +19,24 @@ class Test_Calculador(TestCase):
         print ("Desconto calculado =", desconto)
         
         self.assertEqual(desconto, 38.50000000000001)
+
+    def test_Classe_Calculador_de_imposto(self):
+        from Calculador_de_impostos import Calculador_de_impostos
+        from Orcamento import Orcamento, Item
+        from Impostos import ISS, ICMS, ICPP, IKCV
+
+        calculador = Calculador_de_impostos()
+        orcamento = Orcamento()
+        orcamento.adiciona_itens(Item('ITEM -1', 50))
+        orcamento.adiciona_itens(Item('ITEM -2', 200)) 
+        orcamento.adiciona_itens(Item('ITEM -3', 250))
+
+        print("ISS e ICMS")
+        ISS = calculador.realiza_calculo(orcamento, ISS())
+        self.assertEqual(ISS, 50.0)
+        self.assertEqual(calculador.realiza_calculo(orcamento, ICMS()), 30.0)
+        
+        print("ICPP e IKCV")
+        self.assertEqual(calculador.realiza_calculo(orcamento, ICPP()), 25.0)
+        self.assertEqual(calculador.realiza_calculo(orcamento, IKCV()), 30.0)
+        # self.assertTrue(True)
