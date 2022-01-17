f4b1a68dd9489686679c0c0159e6a6caa8ce169d
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-16 21:28:41 -0500

    Implementando Desing Patterns Estados no modo OO e corrigindo classes abstratas

--- Orcamento.py
+++ Orcamento.py
@@ -1,25 +1,96 @@
+from abc import ABC, abstractmethod
+
+class Estado_de_um_orcamento(ABC):
+
+    @abstractmethod
+    def aplica_desconto_extra(self, orcamento):
+        pass
+
+    @abstractmethod
+    def aprova(self, orcamento):
+        pass
+
+    @abstractmethod
+    def reprova(self, orcamento):
+        pass
+
+    @abstractmethod
+    def finaliza(self, orcamento):
+        pass
+
+class Em_aprovacao(Estado_de_um_orcamento):
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.02)
+
+    def aprova(self, orcamento):
+        orcamento.estado_atual = Aprovado()
+
+    def reprova(self, orcamento):
+        orcamento.estado_atual = Reprovado()
+
+    def finaliza(self, orcamento):
+        raise Exception ("Orcamento em aprovacao nao pode ir para finalizado")
+
+class Aprovado(Estado_de_um_orcamento):
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.05)
+
+    def aprova(self, orcamento):
+        raise Exception ("Orcamento ja esta aprovado")
+
+    def reprova(self, orcamento):
+        raise Exception ("Orcamento aprovado nao pode ser reprovado")
+
+    def finaliza(self, orcamento):
+        orcamento.estado_atual = Finalizado()
+
+class Reprovado(Estado_de_um_orcamento):
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception ("Orcamentos reprovados nao recebem descontos extra")
+
+    def aprova(self, orcamento):
+        raise Exception ("Orcamento reprovado nao pode ser aprovado")
+
+    def reprova(self, orcamento):
+        raise Exception ("Orcamento ja esta reprovado")
+
+    def finaliza(self, orcamento):
+        Orcamento.estado_atual = Finalizado()
+
+class Finalizado(Estado_de_um_orcamento):
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception ("Orcamentos finalizados nao recebem desconto extra")
+
+    def aprova(self, orcamento):
+        raise Exception ("Orcamento finalizado nao pode ser aprovado")
+
+    def reprova(self, orcamento):
+        raise Exception ("Orcamento finalizado nao pode ser reprovado")
+
+    def finaliza(self, orcamento):
+        raise Exception ("Orcamento ja esta finalizado")
+
 class Orcamento (object):
     
-    EM_APROVACAO = 1
-    APROVADO = 2
-    REPROVADO = 3
-    FINALIZADO = 4
-
-
     def __init__(self):
         self.__itens = []
-        self.estado_atual = Orcamento.EM_APROVACAO
+        self.estado_atual = Em_aprovacao()
         self.__desconto_extra = 0
 
+    def aprova(self):
+        self.estado_atual.aprova(orcamento)
+
+    def reprova(self):
+        self.estado_atual.reprova(orcamento)
+
+    def finaliza(self):
+        self.estado_atual.finaliza(orcamento)
+
+    def adiciona_desconto_extra(self, desconto):
+        self.__desconto_extra += desconto
+
     def aplica_desconto_extra(self):
-        if self.estado_atual == Orcamento.EM_APROVACAO:
-            self.__desconto_extra += self.valor * 0.02
-        elif self.estado_atual == Orcamento.APROVADO:
-            self.__desconto_extra += self.valor * 0.05
-        elif self.estado_atual == Orcamento.REPROVADO:
-            raise Exception ("Orcamentos reprovados nao recebem descontos extra")
-        elif self.estado_atual == Orcamento.FINALIZADO:
-            raise Exception ("Orcamentos finalizados nao recebem desconto extra")
+        self.estado_atual.aplica_desconto_extra(self)
       
     @property
     def valor(self):
@@ -66,11 +137,11 @@ if __name__ == "__main__":
     print ("Orcamento com desconto =", orcamento.valor)
 
     #Mudo de estado para aprovado e verifico o novo orcamento
-    orcamento.estado_atual = Orcamento.APROVADO
+    orcamento.aprova()
     orcamento.aplica_desconto_extra()
     print ("Orcamento com desconto =", orcamento.valor)
 
     #Mudo de estado para finalizado e verifico o novo orcamento
-    orcamento.estado_atual = Orcamento.FINALIZADO
+    orcamento.finaliza()
     orcamento.aplica_desconto_extra()
     print ("Orcamento com desconto =", orcamento.valor)
